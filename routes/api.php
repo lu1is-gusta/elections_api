274f6db28b40b198087f09eb7cff5edf0fb40be4
lu1is-gusta <gustavovacodar@gmail.com>
2026-09-18 12:01:49 -0300

    adjust api routes

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,17 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\V1\CandidacyController;
+use App\Http\Controllers\Api\V1\ElectionController;
+use App\Http\Controllers\Api\V1\ElectoralUnitController;
+use App\Http\Controllers\Api\V1\OfficeController;
+use App\Http\Controllers\Api\V1\PartyController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::prefix('v1')->group(function (): void {
+    Route::get('/elections', [ElectionController::class, 'index']);
+    Route::get('/offices', [OfficeController::class, 'index']);
+    Route::get('/parties', [PartyController::class, 'index']);
+    Route::get('/electoral-units', [ElectoralUnitController::class, 'index']);
+    Route::get('/candidacies', [CandidacyController::class, 'index']);
+    Route::get('/candidacies/{id}', [CandidacyController::class, 'show']);
+});
